e4eedace133b9f478e2ba32f6bd15622279f4cee
Nicholas26-design <52610537+Nicholas26-design@users.noreply.github.com>
2021-05-28 17:39:38 -0700

    Create Web Crawler.R

---
+++ Web Crawler.R
@@ -0,0 +1,3 @@
+# Ok so here's what I want. I want a block of code that when executed, will go to address bar of the current active page and write google.com. 
+
+# An extention that opoens up all favorites in one window or a macro that opens up crunchyroll in a new window. 
